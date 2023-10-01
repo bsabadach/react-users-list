51e3cd16ff39efb6c68c442aca120496eacea4d6
bsabadach <boris.sabadach@gmail.com>
2023-10-01 10:06:27 +0200

    test lint stage 5

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { FC, PropsWithChildren } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
+
 import { ModalProvider } from './common/components/modal/ModalContext'
 
 const queryClient = new QueryClient()
