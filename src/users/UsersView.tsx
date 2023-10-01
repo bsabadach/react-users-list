7d6e4e38ee1bcb6a0ec9040cd2e8f0ca932122a4
bsabadach <boris.sabadach@gmail.com>
2023-10-01 10:21:49 +0200

    add spinner on list

--- src/users/UsersView.tsx
+++ src/users/UsersView.tsx
@@ -4,12 +4,13 @@ import { UsersList } from './components/UsersList'
 import { UsersModal } from './components/UsersModal'
 import { useModalContext } from '../common/components/modal/ModalContext'
 import { useUsers } from './model/useUser'
+import { BlockUI } from '../common/components/uiblocker/BlockUI'
 
 export const UsersView: FC = () => {
   const { open } = useModalContext()
   const { usersResult, selectedUserId } = useUsers()
 
-  const { data: users } = usersResult
+  const { data: users, isLoading } = usersResult
 
   const handleSelectUser = (userId: string) => {
     selectedUserId.current = userId
@@ -18,7 +19,9 @@ export const UsersView: FC = () => {
 
   return (
     <>
-      <UsersList onSelectUser={handleSelectUser} users={users ?? []} />
+      <BlockUI when={isLoading}>
+        <UsersList onSelectUser={handleSelectUser} users={users ?? []} />
+      </BlockUI>
       <UsersModal selectedUserId={selectedUserId.current} />
     </>
   )
