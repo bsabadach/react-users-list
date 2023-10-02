54b672a6b16123bdf3d19b6bd71a557f87b0c7e8
bsabadach <boris.sabadach@gmail.com>
2023-10-02 14:21:55 +0200

    add ts-config for integration tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["./tsconfig.json"],
+    project: ["./tsconfig.json","./integration/tsconfig.json"],
     sourceType: 'module'
   },
   plugins: [
